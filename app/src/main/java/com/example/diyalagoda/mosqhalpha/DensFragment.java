3534619dccb529b21270ddbc895c0cd92e3c93c2
gayancliyanage <gayanc@outlook.com>
2016-09-05 19:14:28 +0800

    initial commit

---
+++ app/src/main/java/com/example/diyalagoda/mosqhalpha/DensFragment.java
@@ -0,0 +1,227 @@
+package com.example.diyalagoda.mosqhalpha;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link DensFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link DensFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DensFragment extends ListFragment implements AdapterView.OnItemClickListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    private FragmentActivity myContext;
+    // Array of strings storing country names
+    String[] countries = new String[] {
+            "India",
+            "Pakistan",
+            "Sri Lanka",
+            "China",
+            "Bangladesh",
+            "Nepal",
+            "Afghanistan",
+            "North Korea",
+            "South Korea",
+            "Japan"
+    };
+
+    // Array of integers points to images stored in /res/drawable/
+    int[] flags = new int[]{
+            R.drawable.ic_menu_camera,
+            R.drawable.ic_menu_camera,
+            R.drawable.ic_menu_camera,
+            R.drawable.ic_menu_camera,
+            R.drawable.ic_menu_camera,
+            R.drawable.ic_menu_camera,
+            R.drawable.ic_menu_camera,
+            R.drawable.ic_menu_camera,
+            R.drawable.ic_menu_camera,
+            R.drawable.ic_menu_camera
+    };
+
+    // Array of strings to store currencies
+    String[] currency = new String[]{
+            "Indian Rupee",
+            "Pakistani Rupee",
+            "Sri Lankan Rupee",
+            "Renminbi",
+            "Bangladeshi Taka",
+            "Nepalese Rupee",
+            "Afghani",
+            "North Korean Won",
+            "South Korean Won",
+            "Japanese Yen"
+    };
+
+    public DensFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment DensFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DensFragment newInstance(String param1, String param2) {
+        DensFragment fragment = new DensFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+//        View rootView = inflater.inflate(R.layout.fragment_dens, container, false);
+//        ListView t2 = (ListView) rootView.findViewById(R.id.dens_list);
+//        String[] values = new String[] { "Message1", "Message2", "Message3" };
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+//                R.layout.list_row, values);
+//        t2.setAdapter(adapter);
+//        return  rootView;
+
+        // Each row in the list stores country name, currency and flag
+        List<HashMap<String,String>> aList = new ArrayList<HashMap<String,String>>();
+
+        for(int i=0;i<10;i++){
+            HashMap<String, String> hm = new HashMap<String,String>();
+            hm.put("txt", "Country : " + countries[i]);
+            hm.put("cur","Currency : " + currency[i]);
+            hm.put("flag", Integer.toString(flags[i]) );
+            aList.add(hm);
+        }
+
+        // Keys used in Hashmap
+        String[] from = { "flag","txt","cur" };
+
+        // Ids of views in listview_layout
+        int[] to = { R.id.flag,R.id.txt,R.id.cur};
+
+        // Instantiating an adapter to store each items
+        // R.layout.listview_layout defines the layout of each item
+        SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(), aList, R.layout.list_view_layout, from, to);
+
+        setListAdapter(adapter);
+
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        myContext=(FragmentActivity) activity;
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+
+//        DensDetailsFragment fragment2 = new DensDetailsFragment();
+//        FragmentManager fragmentManager = getFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.replace(R.id.your_placeholder, fragment2);
+//        fragmentTransaction.commit();
+
+        Intent detailsActivity = new Intent(getContext(), DenDetailActivity.class);
+        startActivity(detailsActivity);
+    }
+}
